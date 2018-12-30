8715c900cc114f1318fa4465c009b95e844484fa
Brian MacPherson <brimac1634@gmail.com>
2018-12-30 15:09:17 +0800

    Styled slide menu

--- src/Components/SlideMenu.js
+++ src/Components/SlideMenu.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './SlideMenu.css';
 
 const SlideMenu = () => {
 	return (
 		<div className='slide-menu'>
+			<div className='menu-logo'><h3>Brian</h3></div>
+			<div className='separator'></div>
 			<div className='nav-list'>
 			    <Link to={'/'} className='link' title='About'>About.</Link>
 			    <Link to={'/Projects'} className='link' title='Projects'>Projects.</Link>
 			    <Link to={'/Contact'} className='link' title='Contact'>Contact.</Link>
+			</div>
+			<div className='separator'></div>
+			<div className='contact-info'>
+				<p><strong>Brian MacPherson</strong></p>
+				<p>brimac1634@gmail.com</p>
+				<p>+852-6897-2280</p>
 			</div>
 		</div>
 	);
